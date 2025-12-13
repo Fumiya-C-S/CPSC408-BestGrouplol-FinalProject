ad4eed38b204a6c526092fc555b2be226c13b787
MiguelTel <116687541+MiguelTel@users.noreply.github.com>
2025-12-12 22:27:52 -0800

    Comment Orders.jsx

--- FinalProjDB/db-project/src/Orders.jsx
+++ FinalProjDB/db-project/src/Orders.jsx
@@ -2,29 +2,31 @@ import NavBar from "./NavBar";
 import {useState, useEffect} from "react"
 
 function Orders() {
-    const getOrdersUrl = "http://127.0.0.1:8000/orders/"
-    const [myOrder, setMyOrder] = useState([]);
+    const getOrdersUrl = "http://127.0.0.1:8000/orders/" // Orders url
+    const [myOrder, setMyOrder] = useState([]); //myOrder variable and set function. 
     
-    useEffect(() => {
+    useEffect(() => { //Useeffect to fetch orders
         const fetchOrder = async() => {
-            const userResponse = await fetch(getOrdersUrl)
-            const myOrder = await userResponse.json()
+            const userResponse = await fetch(getOrdersUrl) //Awaits fetch response
+            const myOrder = await userResponse.json() //Await response to be in json format
             console.log(myOrder)
-            setMyOrder(myOrder)
+            setMyOrder(myOrder) //Sets myOrder
         }
-        fetchOrder()
+        fetchOrder() //Fetches the otder
     }, [])
     
     return (
         <>
-        <NavBar></NavBar>
+        <NavBar></NavBar> {/* Navigation bar component */}
+        {/* Returns a div of order list items that are in a order container*/}
         <div style={{ padding: '40px', maxWidth: '1200px', margin: '0 auto' }}>
             <h1>Orders</h1>
             <div className='orders-container'>
-                {myOrder.map((order) => (
+                {myOrder.map((order) => ( //Maps the orders to each order item
                     <div 
                         key={order.OrderID} 
                         className="order-list-item"
+                        //Styles our div of items
                         style={{
                             border: '1px solid #ddd',
                             padding: '20px',
@@ -32,7 +34,7 @@ function Orders() {
                             borderRadius: '8px',
                             backgroundColor: '#f9f9f9'
                         }}
-                    > 
+                    >   {/*Styles our info of the order */}
                         <p style={{ margin: 0, fontSize: '16px' }}>
                             <span style={{ fontWeight: 'normal' }}>Order Number: </span>
                             <b>{order.OrderID}</b>
