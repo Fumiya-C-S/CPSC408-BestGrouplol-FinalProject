e964e97c9829a0bc2963f990572ac5334bfbf96b
MiguelTel <116687541+MiguelTel@users.noreply.github.com>
2025-12-12 22:14:03 -0800

    Comment ItemCard.jsx

--- FinalProjDB/db-project/src/ItemCard.jsx
+++ FinalProjDB/db-project/src/ItemCard.jsx
@@ -1,6 +1,10 @@
 import Header from './Header.jsx'
 import AddButton from './addButton.jsx';
 function ItemCard(props) {
+      //Item card component that takes in props as an arguement. 
+      //Returns a div of an item card. 
+      //It indcludes a header component and an addbutton component. 
+      //Fills in the necessary information based on what is passed to the props. 
       return(
   <div className = 'item-card'>
   <Header title = {props.title} description = {props.description} price = {props.price} productID = {props.productID} category = {props.category}> 
