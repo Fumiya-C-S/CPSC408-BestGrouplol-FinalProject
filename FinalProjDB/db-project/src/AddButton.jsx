9281b28e10d54077f8fad77b969e198e6a842454
jeffreybok <bok@chapman.edu>
2025-12-12 22:39:48 -0800

    fixed addbutton.jsx

--- FinalProjDB/db-project/src/AddButton.jsx
+++ FinalProjDB/db-project/src/AddButton.jsx
@@ -1,5 +1,12 @@
 function AddButton(props) {
-    {/* Button to add to cart. Passed in an onClick function with other arguements using props like productId, title..etc*/}
-    return(<button onClick = {() => props.onClick({productID: props.productID, title: props.title, description: props.description, category: props.category, price: props.price})} className = 'add-button'>Add to Cart</button>);
-}
-export default AddButton
+    return (
+      <button
+        onClick={() => props.onClick(props.productID)}
+        className="add-button"
+      >
+        Add to Cart
+      </button>
+    );
+  }
+  
+  export default AddButton;
